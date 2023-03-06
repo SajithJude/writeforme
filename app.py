93ec200c2c2c82fa371b641d0e76c924515718df
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 19:22:35 +0000

    sss

--- app.py
+++ app.py
@@ -90,15 +90,19 @@ border: 1px solid rgba(255, 255, 255, 0.3);
 # col1, col2,= st.columns(2)
 
 # with col1:
-#     if choose == "Write For Me":
-#         with st.form(key="form"):
-#             st.subheader('Write for me')
-#             des=st.text_input(label='Description') 
-#             # para=st.text_input(label='Parameter') 
-#             submitted = st.form_submit_button('Submit')
-#             if submitted:
-#                 x = openai(des)
-                 
+if choose == "Write For Me":
+
+    navbar = option_menu("Write For me", [""],
+                        #  icons=['cpu', 'lightbulb fill', 'bar-chart fill', 'book','person dash'],
+                         menu_icon="lightbulb-fill", default_index=0, orientation='horizontal'
+                         styles={
+        "container": {"padding": "5!important","background": "rgba(255, 255, 255, 0.2)","border-radius": "16px","box-shadow": "0 4px 30px rgba(0, 0, 0, 0.1)","backdrop-filter": "blur(5px)","-webkit-backdrop-filter": "blur(5px)","border": "1px solid rgba(255, 255, 255, 0.3)"},
+        "icon": {"font-color":"Grey", "font-size": "25px", "border-radius":"50%"}, 
+        "nav-link": {"font-size": "16px","font-weight":"700", "border-radius":"10px", "font-color":"Grey", "text-align": "left", "margin":"10px", "--hover-color": "#FF69B4", "--hover-box-shadow": "0 0 35px rgba(145, 92, 182, .4)"},
+        "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #f63633);"},
+    }
+    )
+        
     
 
 if choose == "Idea Generator":
