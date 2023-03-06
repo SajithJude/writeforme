62962d7fe26b21a9158a27bfafc29157dce1bb6a
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 12:18:14 +0000

    ddd

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with st.container():
                          icons=['user'],
                          menu_icon="app-indicator", default_index=0,
                         styles={
-        "container":{"background-color":"rgba(0, 0, 0, 0)"}
+        "container": "container": {"padding": "5!important","margin": "1!important" ,"background-color": "#fafafa", "box-shadow": "0px 0px 20px 5px #FF69B4"},
     }
     )
 
