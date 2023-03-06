336c312445e4e0d90b2a767ca882616da8267463
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 17:51:26 +0000

    sss

--- app.py
+++ app.py
@@ -13,18 +13,6 @@ st.set_page_config(page_title="My App", page_icon=":rocket:", layout="centered",
 #         """
 # st.markdown(hide_menu_style, unsafe_allow_html=True)
 with st.sidebar:
-    navbar = option_menu("Out put", ["Write For Me", "Idea Generator", "Promotion Ideas", "Account", "Log Out"],
-                         icons=['cpu', 'lightbulb fill', 'bar-chart fill', 'book','person dash'],
-                         menu_icon="app-indicator", default_index=0, orientation='horizontal',
-                         styles={
-        "container": {"padding": "5!important","background": "rgba(255, 255, 255, 0.2)","border-radius": "16px","box-shadow": "0 4px 30px rgba(0, 0, 0, 0.1)","backdrop-filter": "blur(5px)","-webkit-backdrop-filter": "blur(5px)","border": "1px solid rgba(255, 255, 255, 0.3)"},
-        "icon": {"font-color":"Grey", "font-size": "25px", "border-radius":"50%"}, 
-        "nav-link": {"font-size": "16px","font-weight":"700", "border-radius":"10px", "font-color":"Grey", "text-align": "left", "margin":"10px", "--hover-color": "#FF69B4", "--hover-box-shadow": "0 0 35px rgba(145, 92, 182, .4)"},
-        "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #f63633);"},
-    }
-    )
-
-
 
 
     choose = option_menu("App Gallery", ["Write For Me", "Idea Generator", "Promotion Ideas", "Account", "Log Out"],
@@ -49,6 +37,37 @@ with st.sidebar:
         "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #f63633);"},
     }
     )
+
+    if choose == "Write For Me":
+    # st.stop()
+    with st.form(key="form"):
+        st.subheader('Write for me')
+        des=st.text_input(label='Description') 
+        para=st.text_input(label='Parameter') 
+        submitted = st.form_submit_button('Submit')
+        if submitted:
+            st.write('Submitted!')
+            st.stop()
+
+    elif choose == "Idea Generator":
+        with st.form(key="form2"):
+            st.subheader('Idea Generator')
+            des2=st.text_input(label='Description') 
+            para2=st.text_input(label='Parameter') 
+            submitted = st.form_submit_button('Submit')
+            if submitted:
+                st.write('Submitted!')
+                st.stop()
+
+    elif choose == "Promotion Ideas":
+        with st.form(key="form2"):
+            st.subheader('Promotion Ideas')
+            des2=st.text_input(label='Description') 
+            para2=st.text_input(label='Parameter') 
+            submitted = st.form_submit_button('Submit')
+            if submitted:
+                st.write('Submitted!')
+                st.stop()
 
 
 
