96abaf8a82c16807762b32d5d384395841ccf30f
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 19:17:13 +0000

    sss

--- app.py
+++ app.py
@@ -101,16 +101,16 @@ border: 1px solid rgba(255, 255, 255, 0.3);
                  
     
 
-#     elif choose == "Idea Generator":
-#         with st.form(key="form2"):
-#             st.subheader('Idea Generator')
-#             des2=st.text_input(label='Description') 
-#             para2=st.text_input(label='Parameter') 
-#             submitted = st.form_submit_button('Submit')
-#             if submitted:
-#                 x = openai(des2)
-#                 # if "x" not in st.session_state:
-#                 st.session_state['x_result'] = x
+    elif choose == "Idea Generator":
+        with st.form(key="form2"):
+            st.subheader('Idea Generator')
+            des2=st.text_input(label='Description') 
+            para2=st.text_input(label='Parameter') 
+            submitted = st.form_submit_button('Submit')
+            if submitted:
+                x = openai(des2)
+                # if "x" not in st.session_state:
+                # st.session_state['x_result'] = x
 
 
 
