745f1bacebd28340144be28d56b11136715c9bca
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 10:01:36 +0000

    ddd

--- app.py
+++ app.py
@@ -17,5 +17,25 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
+    
+    st.markdown("""
+    <style>
+        .glass-card {
+            background-color: rgba(255, 255, 255, 0.2);
+            box-shadow: 0 0 20px rgba(255, 255, 255, 0.5) inset, 0 0 20px rgba(0, 0, 0, 0.2);
+            border-radius: 20px;
+            padding: 20px;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+    <div class="glass-card">
+        <h1>Transparent Glassmorphic Streamlit Card</h1>
+        <p>This is an example of a transparent Glassmorphic Streamlit card.</p>
+    </div>
+    """, unsafe_allow_html=True)
 add_bg_from_local('images.jpg')    
+
+
     
