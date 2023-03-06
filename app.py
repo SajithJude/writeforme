d9f755c796516fecd3c40dfc40661c79d5afa4aa
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 11:21:19 +0000

    sss

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 
 import base64
 import streamlit as st
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
 
 
+with st.sidebar:
+    choose = option_menu("App Gallery", ["Write For Me", "Idea Generator", "Promotion Ideas", "Account", "Log Out"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
