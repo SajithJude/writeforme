c7b460541f6af47c26b23bc5a1f6e5d1b5f6dca5
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 13:39:50 +0000

    ssss

--- app.py
+++ app.py
@@ -51,15 +51,15 @@ add_bg_from_local('image.jpg')
     
 
 if choose == "Write For Me":
-    st.markdown(""" <style> .font {
-    font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font">Write For Me</p>', unsafe_allow_html=True)
+    # st.markdown(""" <style> .font {
+    # font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    # </style> """, unsafe_allow_html=True)
+    # st.markdown('<p class="font">Write For Me</p>', unsafe_allow_html=True)
     with st.form(key='columns_in_form2',clear_on_submit=True): #set clear_on_submit=True so that the form will be reset/cleared once it's submitted
         #st.write('Please help us improve!')
-        Name=st.text_input(label='Description') #Collect user feedback
-        Email=st.text_input(label='Parameter') #Collect user feedback
-        Message=st.text_input(label='Please Enter Your Message') #Collect user feedback
+        des=st.text_input(label='Description') #Collect user feedback
+        para=st.text_input(label='Parameter') #Collect user feedback
+        # Message=st.text_input(label='Please Enter Your Message') #Collect user feedback
         submitted = st.form_submit_button('Submit')
         if submitted:
             st.write('Submitted!')
