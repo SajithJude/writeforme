595d43eb908b0b585eecce9bccd7375fea9752cf
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 16:44:05 +0000

    ddd

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ elif choose == "Promotion Ideas":
                         styles={
         "container": {"padding": "5!important","background": "rgba(255, 255, 255, 0.2)","border-radius": "16px","box-shadow": "0 4px 30px rgba(0, 0, 0, 0.1)","backdrop-filter": "blur(5px)","-webkit-backdrop-filter": "blur(5px)","border": "1px solid rgba(255, 255, 255, 0.3)"},
         "icon": {"font-color":"Grey", "font-size": "25px", "border-radius":"50%"}, 
-    )
+   
+   } )
