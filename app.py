7eaa0324c4255129508465ecb3dd7cd5f8970cc1
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 15:43:52 +0000

    jjjx

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit.components.v1 as html
 from streamlit_card import card
 
 
-st.set_page_config(page_title="My App", page_icon=":rocket:", layout="wide",initial_sidebar_state="expanded",hide_menu=True  )
+st.set_page_config(page_title="My App", page_icon=":rocket:", layout="wide",initial_sidebar_state="expanded"  )
 
 
 
