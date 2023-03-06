4dc1d3f288765ab22512a416fe4726bfc53c6335
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 09:39:19 +0000

    sss

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: linear-gradient(to bottom right, #00ffff, #ff00ff);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Add your Streamlit app code here
