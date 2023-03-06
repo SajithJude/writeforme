b0bfdb13c496e3cb8e7b8fe109de38afecb4f48b
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 10:51:41 +0000

    SSS

--- app.py
+++ app.py
@@ -17,21 +17,15 @@ def add_bg_from_local(image_file):
         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
     }}
 
-    .card  {
+    .card  {{
             display: flex;
-            justify-content: center;
-            align-items: center;
-            background-size: cover;
             height: 300px;
             width: 500px;
              border-radius: 20px;
-
-            background-position: center;
             box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
             
-            font-size: 24px;
-            text-align: center;
-    }
+        
+    }}
     </style>
     """,
     unsafe_allow_html=True
