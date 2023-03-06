b048796e6f340b26cb4fcc351279b961c264fe62
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 15:54:01 +0000

    nnnn

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with st.sidebar:
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important","margin": "1!important" ,"background-color": "#fafafa", "box-shadow": "0px 0px 20px 5px #FF69B4"},
-        "icon": {"color": "grey", "font-size": "25px", "border-radius":"50%","margin": "2!important"}, 
+        "icon": {"font-color":"Grey", "font-size": "25px", "border-radius":"50%","margin": "2!important"}, 
         "nav-link": {"font-size": "16px","font-weight":"700", "font-color":"Grey", "text-align": "left", "margin":"0px", "--hover-color": "#FF69B4"},
         "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #E91E63);"},
     }
