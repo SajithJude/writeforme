3223187c0538ee8cd09df2bbd29438c4cd8cce0a
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 17:36:28 +0000

    ddd

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ navbar = option_menu("Output", [""],
                          icons=['cpu', 'lightbulb fill', 'bar-chart fill', 'book','person dash'],
                          menu_icon="menu-button", default_index=0, orientation='horizontal',
                          styles={
-        "container": {"padding": "5!important","background": "rgba(255, 255, 255, 0.2)","border-radius": "16px","box-shadow": "0 4px 30px rgba(0, 0, 0, 0.1)","backdrop-filter": "blur(5px)","-webkit-backdrop-filter": "blur(5px)","border": "1px solid rgba(255, 255, 255, 0.3)"},
+        "container": {"background": "rgba(255, 255, 255, 0.2)","border-radius": "16px","box-shadow": "0 4px 30px rgba(0, 0, 0, 0.1);","backdrop-filter": "blur(5px)","-webkit-backdrop-filter": "blur(5px)","border": "1px solid rgba(255, 255, 255, 0.3)"},
         "icon": {"font-color":"Grey", "font-size": "25px", "border-radius":"50%"}, 
         "nav-link": {"font-size": "16px","font-weight":"700", "border-radius":"10px", "font-color":"Grey", "text-align": "left", "margin":"10px", "--hover-color": "#FF69B4", "--hover-box-shadow": "0 0 35px rgba(145, 92, 182, .4)"},
         "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #f63633);"},
