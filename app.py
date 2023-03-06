77b8fe3d92761e8cd0e78c9000c41d875487b8d4
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 11:25:06 +0000

    ss

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ with st.sidebar:
         "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": " linear-gradient(to right, #4B0082, #FF69B4);"},
+        "nav-link-selected": {"background": " linear-gradient(to right, #4B0082, #FF69B4);"},
     }
     )
 
