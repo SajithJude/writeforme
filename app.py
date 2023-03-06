404c0c9fc44418aad62df97622d0e3b8244b9d5a
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-06 11:55:21 +0000

    sss

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with st.sidebar:
                          icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
-        "container": {"padding": "5!important","margin": "5!important" ,"background-color": "#fafafa", "box-shadow": "0px 0px 20px 5px #FF69B4"},
+        "container": {"padding": "10!important","margin": "5!important" ,"background-color": "#fafafa", "box-shadow": "0px 0px 20px 5px #FF69B4"},
         "icon": {"color": "white", "font-size": "25px", "border-radius":"3px","margin": "2!important"}, 
         "nav-link": {"font-size": "16px","font-weight":"700", "font-color":"Grey", "text-align": "left", "margin":"0px", "--hover-color": "#FF69B4"},
         "nav-link-selected": {"background": " linear-gradient(to right, #9B59B6, #E91E63);"},
